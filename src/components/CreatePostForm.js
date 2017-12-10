ca94ff514ec380bf6dc0016bb2c63eb1ab5b5ecb
why950708 <why950708@163.com>
2017-12-10 13:50:09 -0600

    testing

--- src/components/CreatePostForm.js
+++ src/components/CreatePostForm.js
@@ -1,71 +1,68 @@
 import React from 'react';
-import { Form, Modal, Input, Upload, Icon } from 'antd';
+import { Form, Input, Icon, Upload } from 'antd';
 
-export class CreatePostForm extends React.Component {
-    normFile = (e) => {
-        console.log('Upload event:', e);
-        if (Array.isArray(e)) {
-            return e;
-        }
-        return e && e.fileList;
-    }
+const FormItem = Form.Item;
 
-    beforeUpload = () => {
-        return false;
-    }
+class CreatePostForm extends React.Component {
+ beforeUpload = () => {
+   return false;
+ }
 
-    render() {
-        const FormItem = Form.Item;
-        const { visible, onCancel, onCreate, confirmLoading, form } = this.props;
-        const { getFieldDecorator } = form;
-        const formItemLayout = {
-            labelCol: { span: 6 },
-            wrapperCol: { span: 14 },
-        };
-        return (
-            <Modal
-                visible={visible}
-                title="Create A New Post"
-                okText="Create"
-                cancelText="Cancel"
-                onCancel={onCancel}
-                onOk={onCreate}
-                confirmLoading={confirmLoading}
-            >
-                <Form>
-                    <FormItem
-                        {...formItemLayout}
-                        label="Message"
-                    >
-                        {getFieldDecorator('message', {
-                            rules: [{ required: true, message: 'Please input the title of collection!' }],
-                        })(
-                            <Input name="message"/>
-                        )}
-                    </FormItem>
-                    <FormItem
-                        {...formItemLayout}
-                        label="Image"
-                    >
-                        {getFieldDecorator('image', {
-                            valuePropName: 'fileList',
-                            getValueFromEvent: this.normFile,
-                        })(
-                            <Upload.Dragger
-                                name="image"
-                                beforeUpload={this.beforeUpload}
-                            >
-                                <p className="ant-upload-drag-icon">
-                                    <Icon type="inbox" />
-                                </p>
-                                <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                            </Upload.Dragger>
-                        )}
-                    </FormItem>
-                </Form>
-            </Modal>
-        );
-    }
+ normFile = (e) => {
+   console.log('Upload event:', e);
+   if (Array.isArray(e)) {
+     return e;
+   }
+   return e && e.fileList;
+ }
+
+ getWrapperForm = () => {
+   return this.props.form;
+ }
+
+ render() {
+   const { getFieldDecorator } = this.props.form;
+   const formItemLayout = {
+     labelCol: { span: 6 },
+     wrapperCol: { span: 14 },
+   };
+   return (
+     <Form layout="vertical">
+       <FormItem
+         {...formItemLayout}
+         label="Message"
+       >
+         {getFieldDecorator('message', {
+           rules: [{ required: true, message: 'Please input the title of collection!' }],
+         })(
+           <Input />
+         )}
+       </FormItem>
+       <FormItem
+         {...formItemLayout}
+         label="Image"
+       >
+         <div className="dropbox">
+           {getFieldDecorator('image', {
+             rules: [{ required: true, message: 'Please choose an image' }],
+             valuePropName: 'fileList',
+             getValueFromEvent: this.normFile,
+           })(
+             <Upload.Dragger
+               name="files"
+               beforeUpload={this.beforeUpload}
+             >
+               <p className="ant-upload-drag-icon">
+                 <Icon type="inbox" />
+               </p>
+               <p className="ant-upload-text">Click or drag file to this area to upload</p>
+             </Upload.Dragger>
+           )}
+         </div>
+       </FormItem>
+     </Form>
+   );
+ }
 }
 
 export const WrappedCreatePostForm = Form.create()(CreatePostForm);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,118 +1,124 @@
-import React from 'react';
-import {Tabs, Spin} from 'antd';
+import React, { Component } from 'react';
 import $ from 'jquery';
-import {Gallery} from "./Gallery"
-import {CreatePostButton} from "./CreatePostButton"
-import {API_ROOT, AUTH_PREFIX, GEO_OPTIONS, POS_KEY, TOKEN_KEY} from '../constants'
+import { Tabs, Spin } from 'antd';
+import { Gallery } from './Gallery';
+import { WrappedAroundMap } from './AroundMap';
+import { CreatePostButton } from './CreatePostButton';
+import { API_ROOT, TOKEN_KEY, AUTH_PREFIX, GEO_OPTIONS, POS_KEY } from '../constants';
 
 
-const TabPane = Tabs.TabPane;
+export class Home extends Component {
+ state = {
+   posts: [],
+   loadingGeoLocation: false,
+   loadingPosts: false,
+   error: '',
+ }
 
+ onTabChange = (key) => {
+   console.log(key);
+ }
 
-export class Home extends React.Component {
-    state = {
-        posts:[],
-        error : '',
-        loadingGeoLocation: false,
-        loadingPosts: false,
-    }
-    onTabChange = (key) => {
-        console.log(key);
-    }
+ getGeoLocation = () => {
+   if (navigator && navigator.geolocation) {
+     this.setState({ loadingGeoLocation: true, error: '' });
+     navigator.geolocation.getCurrentPosition(
+       this.onSuccessLoadGeoLocation,
+       this.onFailedLoadGeoLocation,
+       GEO_OPTIONS
+     );
+   } else {
+     this.setState({ error: 'Your browser does not support geolocation!'});
+   }
+ }
 
-    componentDidMount() {
-        if ("geolocation" in navigator) {
-            this.setState({loadingGeoLocation:true, error:''});
-            navigator.geolocation.getCurrentPosition(
-                this.onSuccessLoadGeoLocation,
-                this.onFailedLoadGeoLocation,
-                GEO_OPTIONS,
-            );
-        } else {
-            this.setState({error : "geo location not supported"});
-            //const {lat, lon} = {"lat":37.5629917, "lon":-122.3255253999999}
+ onSuccessLoadGeoLocation = (position) => {
+   this.setState({ loadingGeoLocation: false, error: '' });
+   const { latitude: lat, longitude: lon } = position.coords;
+   localStorage.setItem(POS_KEY, JSON.stringify({ lat: lat, lon: lon }));
+   this.loadNearbyPosts();
+ }
 
-        }
-    }
+ onFailedLoadGeoLocation = () => {
+   this.setState({ loadingGeoLocation: false, error: 'Failed to load geo location!' });
+ }
 
-    onSuccessLoadGeoLocation = (position) => {
-        this.setState({loadingGeoLocation:false, error:''});
-        const {latitude: lat, longitude: lon} = position.coords;
-        localStorage.setItem(POS_KEY, JSON.stringify({lat: lat, lon:lon}));
-        console.log(position);
-        this.loadNearbyPosts();
-    }
+ componentDidMount() {
+   this.getGeoLocation();
+ }
 
-    onFailedLoadGeoLocation = (error) => {
-        this.setState({loadingGeoLocation:false, error: "Failed to load Geo location!"});
-    }
+ loadNearbyPosts = () => {
+   const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
+   this.setState({ loadingPosts: true });
+   return $.ajax({
+     url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20`,
+     method: 'GET',
+     headers: {
+       'Authorization': `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`,
+     },
+   }).then((response) => {
+     this.setState({ posts: response, error: '' });
+     console.log(response);
+   }, (error) => {
+     this.setState({ error: error.responseText });
+   }).then(() => {
+     this.setState({ loadingPosts: false });
+   }).catch((error) => {
+     console.log(error);
+   });
+ }
 
-    getGalleryPanelContent = () => {
-            if (this.state.error) {
-                return <div>{this.state.error}</div>
-             }
-        else if (this.state.loadingGeoLocation) {
-            // Show spinner
-            return <Spin tip="Loading geo location ..." size={"large"}/>
-        } else if (this.state.loadingPosts) {
-                // Show spinner
-                return <Spin tip="Loading posts ..." size={"large"}/>
-            } else if (this.state.posts) {
-                // Show gallery
-                const images = this.state.posts.map((post) => {
-                    return {
-                        user: post.user,
-                        src: post.url,
-                        thumbnail: post.url,
-                        thumbnailWidth: 400,
-                        thumbnailHeight: 300,
-                        caption: post.message,
-                    };
+ getGalleryPanelContent = () => {
+   if (this.state.error) {
+     return <div>{this.state.error}</div>
+   } else if (this.state.loadingGeoLocation) {
+     return <Spin tip="Loading geo location ..."/>
+   } else if (this.state.loadingPosts) {
+     return <Spin tip="Loading posts ..."/>
+   } else if (this.state.posts) {
+     const images = this.state.posts.map((post) => {
+       return {
+         user: post.user,
+         src: post.url,
+         thumbnail: post.url,
+         thumbnailWidth: 400,
+         thumbnailHeight: 300,
+         caption: post.message,
+       };
+     });
+     return (
+       <Gallery
+         images={images}
+       />
+     );
+   }
+   return null;
+ }
 
-                });
-                console.log(images);
-                return (<Gallery images={images}/>);
-            }
-        return null;
-    }
-
-    loadNearbyPosts = () => {
-        const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
-        this.setState({ loadingPosts: true });
-        return $.ajax({
-            url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20`,
-            method: 'GET',
-            headers: {
-                Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`,
-            },
-        }).then((response) => {
-            this.setState({ posts: response, error: '' });
-            console.log(response);
-        }, (error) => {
-            this.setState({ error: error.responseText });
-        }).then(() => {
-            this.setState({ loadingPosts: false });
-        }).catch((error) => {
-            console.log(error);
-        });
-    }
-
-    render() {
-        const createPostButton = <CreatePostButton loadNearbyPosts={this.loadNearbyPosts}/>;
-        return (
-            <Tabs
-                onChange={this.onTabChange}
-                tabBarExtraContent={createPostButton}
-                className="main-tabs"
-            >
-                <TabPane tab="Posts" key="1">
-                    {this.getGalleryPanelContent()}
-                </TabPane>
-                <TabPane tab="Map" key="2">
-                    Content Of Tab 2
-                </TabPane>
-            </Tabs>
-        );
-
-    }
+ render() {
+   const TabPane = Tabs.TabPane;
+   const createPostButton = <CreatePostButton loadNearbyPosts={this.loadNearbyPosts}/>;
+   return (
+     <Tabs
+       defaultActiveKey="1"
+       onChange={this.onTabChange}
+       tabBarExtraContent={createPostButton}
+       className="main-tabs"
+     >
+       <TabPane tab="Posts" key="1">
+         {this.getGalleryPanelContent()}
+       </TabPane>
+       <TabPane tab="Map" key="2">
+         <WrappedAroundMap
+           posts={this.state.posts}
+           loadNearbyPosts={this.loadNearbyPosts}
+           googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+           loadingElement={<div style={{ height: `100%` }} />}
+           containerElement={<div style={{ height: `600px` }} />}
+           mapElement={<div style={{ height: `100%` }} />}
+         />
+       </TabPane>
+     </Tabs>
+   );
+ }
 }
